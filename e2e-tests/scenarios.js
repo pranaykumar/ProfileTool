7b5b7d28724f070cb844854c695825f69f9a1f47
pranaykumar <pranay.mishra@gmail.com>
2014-10-08 23:57:07 +0530

    1. Added e2e test case for testing index.html and defualt home page.

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -4,39 +4,39 @@
 
 describe('my app', function() {
 
-  browser.get('index.html');
+  browser.get('#/index.html');
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+  it('should automatically redirect to /providers when location hash/fragment is empty', function() {
+    expect(browser.getLocationAbsUrl()).toMatch("/providers");
   });
 
 
-  describe('view1', function() {
+  describe('provider', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view1');
+      browser.get('#/providers');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
+    it('should render providers when user navigates to #/providers', function() {
+      expect(element.all(by.css('[ng-controller] label')).first().getText()).
+        toMatch('Search Provider');
     });
 
   });
+
+
+//  describe('view2', function() {
+//
+//    beforeEach(function() {
+//      browser.get('index.html#/view2');
+//    });
+//
+//
+//    it('should render view2 when user navigates to /view2', function() {
+//      expect(element.all(by.css('[ng-view] p')).first().getText()).
+//        toMatch(/partial for view 2/);
+//    });
+//
+//  });
 });
