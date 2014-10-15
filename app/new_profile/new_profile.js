3cfe9c105587c196e002509fca17c906990f10e8
pranaykumar <pranay.mishra@gmail.com>
2014-10-15 22:59:52 +0530

    1. Removed profile.json file. 2. Updated new profile save function to show confirmation alert.

--- app/new_profile/new_profile.js
+++ app/new_profile/new_profile.js
@@ -11,8 +11,16 @@ angular.module('profileApp.new_profile', [ 'ngRoute' ])
 	});
 } ])
 
-.controller('newProfileController',
-		function($scope, $routeParams, $http, ProfileService) {
+.controller(
+		'newProfileController',
+		function($scope, $routeParams, $http, ProfileService, $location,
+				$anchorScroll) {
+
+			$scope.confirmationAlert = '';
+			$scope.closeAlert = function() {
+				$scope.confirmationAlert = '';
+			};
+
 			$scope.selectedProvider = $routeParams.provider_id;
 			$scope.templates = [ {
 				'id' : 0,
@@ -31,15 +39,16 @@ angular.module('profileApp.new_profile', [ 'ngRoute' ])
 				"name" : "",
 				"type" : "video",
 				"private" : 0,
-				"deinterlace_input" : null,
+				"deinterlace_input" : 0,
 				"frame_rate" : 60.0,
-				"mezzanine_multipass_encoding" : null,
+				"mezzanine_multipass_encoding" : 0,
 				"image_interval_sec" : null,
 				"custom_image_widths" : "",
-				"override_source" : null,
+				"override_source" : 0,
 				"stream_type" : "",
 				"streams" : []
-			}
+			};
+
 			$scope.selectedTemplate = $scope.templates[0];
 
 			$scope.updateTemplateOption = function(templateIndex) {
@@ -48,20 +57,21 @@ angular.module('profileApp.new_profile', [ 'ngRoute' ])
 
 			$scope.createProfile = function() {
 				console.log($scope.profile);
-				
-				$http(
-						{
-							method : 'POST',
-							url : 'http://' + IP + ':3000/api/profile/',
-							data : {
-								'profile' : $scope.profile,
-								'provider' : $scope.selectedProvider
-							},
-							headers : {
-								'content-type' : 'application/json'
-							}
-						}).success(function(data) {
-					alert(data[0].msg);
+
+				$http({
+					method : 'POST',
+					url : 'http://' + IP + ':3000/api/profile/',
+					data : {
+						'profile' : $scope.profile,
+						'provider' : $scope.selectedProvider
+					},
+					headers : {
+						'content-type' : 'application/json'
+					}
+				}).success(function(data) {
+					$scope.confirmationAlert = data[0].msg;
+//					$location.hash('alert');
+//					$anchorScroll();
 				});
 			};
 		});
